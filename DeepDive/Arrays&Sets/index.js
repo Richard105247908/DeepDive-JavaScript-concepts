d9b2dd783f2af05c41996b710455de602fdfc5d4
Richard105247908 <105247908@aston.ac.uk>
2025-05-14 14:51:38 +0100

    pop

--- DeepDive/Arrays&Sets/index.js
+++ DeepDive/Arrays&Sets/index.js
@@ -3,3 +3,8 @@ favouriteSongs=[];
 favouriteSongs.push("Billy_jean");
 favouriteSongs.push(`This is how we do`);
 favouriteSongs.push(`Dipset`);
+
+console.log(favouriteSongs)
+console.log(favouriteSongs[favouriteSongs.length - 1])
+
+favouriteSongs.pop();
