df3b8f0ab155b33d88c7f20c9440b8601523d2af
Richard105247908 <105247908@aston.ac.uk>
2025-05-19 09:54:13 +0100

    some and every

--- DeepDive/Arrays&Sets/index.js
+++ DeepDive/Arrays&Sets/index.js
@@ -1,12 +1,25 @@
-favouriteSongs=[];
+//Challenge array
+// favouriteSongs=[];
 
-favouriteSongs.push("Billy_jean");
-favouriteSongs.push(`This is how we do`);
-favouriteSongs.push(`Dipset`);
+// favouriteSongs.push("Billy_jean");
+// favouriteSongs.push(`This is how we do`);
+// favouriteSongs.push(`Dipset`);
 
-console.log(favouriteSongs)
-console.log(favouriteSongs[favouriteSongs.length - 1])
+// console.log(favouriteSongs)
+// console.log(favouriteSongs[favouriteSongs.length - 1])
 
-favouriteSongs.pop();
+// favouriteSongs.pop();
 
-console.log(favouriteSongs[favouriteSongs.length - 1])
+// console.log(favouriteSongs[favouriteSongs.length - 1])
+
+//Challenge: .some and .every
+const songs = [
+    {song: "Shape of You", timesStreamed: 2.384, wonGrammy: true},
+    {song: "One Dance", timesStreamed: 1.791, wonGrammy: false},
+    {song: "Rockstar", timesStreamed: 1.781	, wonGrammy: false},
+    {song: "Closer", timesStreamed: 1.688, wonGrammy: false},
+    {song: "Thinking Out Loud", timesStreamed: 1.461, wonGrammy: true}
+]
+
+const result = songs.some(song=>song.wonGrammy)
+console.log(result)
