b9a6f29a58403bd5796ed6b5f6a6141d461d90e7
Richard105247908 <105247908@aston.ac.uk>
2025-05-19 11:14:37 +0100

    total weight of cars using reduce

--- DeepDive/Arrays&Sets/index.js
+++ DeepDive/Arrays&Sets/index.js
@@ -13,17 +13,33 @@
 // console.log(favouriteSongs[favouriteSongs.length - 1])
 
 //Challenge: .some and .every
-const songs = [
-    {song: "Shape of You", timesStreamed: 2.384, wonGrammy: true},
-    {song: "One Dance", timesStreamed: 1.791, wonGrammy: false},
-    {song: "Rockstar", timesStreamed: 1.781	, wonGrammy: false},
-    {song: "Closer", timesStreamed: 1.688, wonGrammy: false},
-    {song: "Thinking Out Loud", timesStreamed: 1.461, wonGrammy: true}
-]
+// const songs = [
+//     {song: "Shape of You", timesStreamed: 2.384, wonGrammy: true},
+//     {song: "One Dance", timesStreamed: 1.791, wonGrammy: false},
+//     {song: "Rockstar", timesStreamed: 1.781	, wonGrammy: false},
+//     {song: "Closer", timesStreamed: 1.688, wonGrammy: false},
+//     {song: "Thinking Out Loud", timesStreamed: 1.461, wonGrammy: true}
+// ]
 
-// const result = songs.some(song=>song.wonGrammy)
+// // const result = songs.some(song=>song.wonGrammy)
+// // console.log(result)
+
+// const allMegaHits = songs.every(song=>song.timesStreamed > 1.5)
 // console.log(result)
 
-const allMegaHits = songs.every(song=>song.timesStreamed > 1.5)
-console.log(result)
+//Challenge: .reduce()
+const cars = [
+  { name: "Toyota", isElectric: false, weight: 1320 },
+  { name: "Ford", isElectric: false, weight: 1400 },
+  { name: "Volkswagen", isElectric: false, weight: 1370 },
+  { name: "Honda", isElectric: false, weight: 1375 },
+  { name: "Tesla", isElectric: true, weight: 1750 },
+  { name: "BMW", isElectric: true, weight: 1350 },  
+];
 
+const total= cars.reduce((accumulator, carItems)=>{
+    return accumulator + carItems.weight
+
+},0);
+
+console.log(total)
