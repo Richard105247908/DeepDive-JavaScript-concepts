41184dbe6f49b130702cbc584477071e0567d0af
Richard105247908 <105247908@aston.ac.uk>
2025-05-28 16:40:58 +0100

    DOM challenge

--- DeepDive/DOM/index.js
+++ DeepDive/DOM/index.js
@@ -7,3 +7,14 @@ p.style.color="#05f"
 p.style.fontSize="x-large";
 
 p.addEventListener("click", () => console.log('clicked'))
+
+
+const title = document.querySelector('h1');
+title.innerHTML = "Creating and modifying HTML elements";
+
+const tagline = document.createElement('h2');
+tagline.className = 'tagline';
+tagline.innerHTML = 'I can create HTML elements!';
+
+title.append(tagline);
+
