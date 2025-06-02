967cfe503f65e2fd3200ef171e1e5ddb32f6a547
Richard105247908 <105247908@aston.ac.uk>
2025-06-02 10:23:37 +0100

    challenge 2 count downwards

--- DeepDive/Functions/index.js
+++ DeepDive/Functions/index.js
@@ -12,3 +12,21 @@ function splitBill(amount,people){
 console.log(splitBill(10, 2))
 console.log(splitBill(16, 4))
 console.log(splitBill(350, 9))
+
+
+
+// Start
+function countdown(minus=10) {
+  let value=100;
+  // write your code here
+  return function decrease(){
+    value -= minus
+    return value
+  }
+}
+
+const countingDown = countdown(8);
+
+console.log(countingDown());
+console.log(countingDown());
+console.log(countingDown());
